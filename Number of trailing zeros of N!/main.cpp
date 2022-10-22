b2b633ce66546141963024899b20ba0a06e986ac
KarolDawidG <karoldawidg@gmail.com>
2022-10-22 15:36:57 +0200

    LOG 6: For now, I have only N!

---
+++ Number of trailing zeros of N!/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+#include <vector>
+#include <bits/stdc++.h>
+
+long zeros(long n)
+{
+long silnia {1};
+for (long i = 1; i <= n; i++)
+    silnia =+ silnia*i;
+
+
+return silnia2;
+}
+
+int main()
+{
+int n{};
+std::cout<<"Type some number: ";
+std::cin>>n;
+
+std::cout<<"Result: "<<zeros(n)<<std::endl;
+     return 0;
+}
