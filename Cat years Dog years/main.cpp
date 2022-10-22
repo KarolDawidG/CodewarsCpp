5c4ce7f46b8ad5de330c56f88e7d54e15d1bfcc0
KarolDawidG <karoldawidg@gmail.com>
2022-10-22 18:44:14 +0200

    LOG 10: Cat and dogs: everything worked perfectly

--- Cat years Dog years/main.cpp
+++ Cat years Dog years/main.cpp
@@ -1,36 +1,43 @@
 #include <iostream>
-#include <cstdlib>
-#include <math.h>
 #include <vector>
-#include <bits/stdc++.h>
 
-//std::vector<int> humanYearsCatYearsDogYears(int humanYears){
-int humanYearsCatYearsDogYears(int humanYears){
-//int dogYears{};
+
+std::vector<int> humanYearsCatYearsDogYears(int humanYears){
+int dogYears{};
 int catYears{};
 int year{};
+std::vector<int>years;
 
     for (year = 1; year <= humanYears; year++){
         if (year == 1){
             catYears += 15;
-           // dogYears += 15;
+            dogYears += 15;
         }else if(year == 2){
             catYears += 9;
-          //  dogYears += 9;
+            dogYears += 9;
         }else{
             catYears += 4;
-         //   dogYears += 4;
+            dogYears += 5;
         }
     }
-        return catYears;
+        years.push_back(humanYears);
+        years.push_back(catYears);
+        years.push_back(dogYears);
+        return years;
     }
 
 
 int main()
 {
 int humanYears{};
-std::cout<<"How long do you have your pets? ";
+std::cout<<"How old are your pets? ";
 std::cin>>humanYears;
-std::cout<<"Pet has "<<humanYearsCatYearsDogYears(humanYears)<<std::endl;
+
+std::vector<int>yearsOfYourPets;
+yearsOfYourPets = humanYearsCatYearsDogYears(humanYears);
+std::cout<<"Your are "<<yearsOfYourPets[0]<<" years old"<<std::endl;
+std::cout<<"Your cat is "<<yearsOfYourPets[1]<<" years old"<<std::endl;
+std::cout<<"Your dog is "<<yearsOfYourPets[2]<<" years old"<<std::endl;
+
      return 0;
 }
