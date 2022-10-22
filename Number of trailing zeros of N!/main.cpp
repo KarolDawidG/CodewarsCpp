de396776fe33e19e9da31b585ee18e7573cf2921
KarolDawidG <karoldawidg@gmail.com>
2022-10-22 16:19:26 +0200

    LOG 7: zeros of N!

--- Number of trailing zeros of N!/main.cpp
+++ Number of trailing zeros of N!/main.cpp
@@ -9,9 +9,17 @@ long zeros(long n)
 long silnia {1};
 for (long i = 1; i <= n; i++)
     silnia =+ silnia*i;
+return silnia;
+}
 
-
-return silnia2;
+int findZero(int n){
+int ans{};
+int p=5;
+    while((n/p)>0){
+        ans += (n/p);
+        p = p*5;
+        }
+    return ans;
 }
 
 int main()
@@ -20,6 +28,7 @@ int n{};
 std::cout<<"Type some number: ";
 std::cin>>n;
 
-std::cout<<"Result: "<<zeros(n)<<std::endl;
+std::cout<<"Result of N!: "<<zeros(n)<<std::endl;
+std::cout<<"Number of zeros: "<<findZero(n)<<std::endl;
      return 0;
 }
