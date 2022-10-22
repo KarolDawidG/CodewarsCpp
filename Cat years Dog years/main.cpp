af97b77cb5826fdf829564b3499bdac2126f83b6
KarolDawidG <karoldawidg@gmail.com>
2022-10-22 21:34:59 +0200

    LOG 11: Upgrade Cat&Dog: more operations on the vectors

--- Cat years Dog years/main.cpp
+++ Cat years Dog years/main.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
 #include <vector>
-
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <iterator>
 
 std::vector<int> humanYearsCatYearsDogYears(int humanYears){
-int dogYears{};
-int catYears{};
-int year{};
-std::vector<int>years;
-
-    for (year = 1; year <= humanYears; year++){
+std::vector<int>years{0,0,0};
+    for (int year = 1; year <= humanYears; year++){
         if (year == 1){
-            catYears += 15;
-            dogYears += 15;
+            years.at(0) = humanYears;
+            years.at(1) = 15;
+            years.at(2) = 15;
         }else if(year == 2){
-            catYears += 9;
-            dogYears += 9;
+            years.at(1)= (years[1])+9;
+            years.at(2)= (years[2])+9;
         }else{
-            catYears += 4;
-            dogYears += 5;
+          years.at(1)  = (years[1])+4;
+          years.at(2)  = (years[2])+5;
         }
     }
-        years.push_back(humanYears);
-        years.push_back(catYears);
-        years.push_back(dogYears);
         return years;
     }
+std::vector<int>printVector(std::vector<int>yearsOfYourPets){
+ for (int i = 0; i < yearsOfYourPets.size(); i++) {
+        std::cout << yearsOfYourPets.at(i);
+        std::cout << " ";
+    }
+}
 
 
 int main()
@@ -32,12 +34,16 @@ int main()
 int humanYears{};
 std::cout<<"How old are your pets? ";
 std::cin>>humanYears;
-
 std::vector<int>yearsOfYourPets;
 yearsOfYourPets = humanYearsCatYearsDogYears(humanYears);
 std::cout<<"Your are "<<yearsOfYourPets[0]<<" years old"<<std::endl;
 std::cout<<"Your cat is "<<yearsOfYourPets[1]<<" years old"<<std::endl;
 std::cout<<"Your dog is "<<yearsOfYourPets[2]<<" years old"<<std::endl;
+std::cout<<std::endl;
+
+printVector(yearsOfYourPets);
+
+
 
      return 0;
 }
